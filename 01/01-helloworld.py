aeb7a6b86d7821726ee31d5516dcd0cab05d61b8
Kim Woog Lae <kimwooglae@gmail.com>
2016-11-14 22:48:57 +0900

    add example

--- 01/01-helloworld.py
+++ 01/01-helloworld.py
@@ -2,6 +2,19 @@ import tensorflow as tf
 
 hello = tf.constant('Hello, TensorFlow!!!')
 
+print hello
+
 session = tf.Session()
 
 print session.run(hello)
+
+a = tf.constant(2)
+b = tf.constant(3)
+
+c =  a + b
+
+print a
+print b
+print c
+
+print session.run(c);
