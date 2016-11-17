db315766e45de9250e763c004410fc71c57a7ca3
Kim Woog Lae <kimwooglae@gmail.com>
2016-11-18 06:46:12 +0900

    logistic classification

--- 04/02.multivariable_array.py
+++ 04/02.multivariable_array.py
@@ -1,12 +1,16 @@
 import tensorflow as tf
 
 x_data = [[1,1,1,1,1],[1.,0.,3.,0.,5.],[0.,2.,0.,4.,0.]]
+#x_data = [[1.,0.,3.,0.,5.],[0.,2.,0.,4.,0.]]
 y_data = [1,2,3,4,5]
 
 W =tf.Variable(tf.random_uniform([1,3], -1.0, 1.0))
+#W =tf.Variable(tf.random_uniform([1,2], -1.0, 1.0))
+#b =tf.Variable(tf.random_uniform([1], -1.0, 1.0))
 
 
 hypothesis = tf.matmul(W, x_data)
+#hypothesis = tf.matmul(W, x_data) + b
 
 
 cost = tf.reduce_mean(tf.square(hypothesis - y_data))
@@ -24,3 +28,4 @@ for step in xrange(2001):
 	sess.run(train)
 	if step % 20 == 0:
 		print step, sess.run(cost), sess.run(W)
+#		print step, sess.run(cost), sess.run(W), sess.run(b)
