605e9b8b528e0ca36d3fe5e5daa18c3fe5669b90
Kim Woog Lae <kimwooglae@gmail.com>
2016-11-23 01:05:22 +0900

    regression

--- 02/01.regression.py
+++ 02/01.regression.py
@@ -3,12 +3,14 @@ import tensorflow as tf
 x_data = [1,2,3]
 y_data = [1,2,3]
 
+X = tf.placeholder(tf.float32)
+Y = tf.placeholder(tf.float32)
 W = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
 b = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
 
-hypothesis = W * x_data + b
+hypothesis = W * X+ b
 
-cost = tf.reduce_mean(tf.square(hypothesis - y_data))
+cost = tf.reduce_mean(tf.square(hypothesis - Y))
 
 a = tf.Variable(0.1)
 optimizer = tf.train.GradientDescentOptimizer(a)
@@ -20,6 +22,6 @@ sess = tf.Session()
 sess.run(init)
 
 for step in xrange(2001):
-	sess.run(train)
+	sess.run(train, feed_dict={X:x_data, Y:y_data})
 	if step % 20 == 0:
-		print step, sess.run(cost), sess.run(W), sess.run(b)
+		print step, sess.run(cost, feed_dict={X:x_data, Y:y_data}), sess.run(W), sess.run(b)
